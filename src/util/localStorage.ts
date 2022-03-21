7dd803cf71671bc4aa7d6238ff8e6c301aad3a26
Assar <33634310+assarbertil@users.noreply.github.com>
2022-03-21 19:38:33 +0100

    local storage functions are done

--- src/util/localStorage.ts
+++ src/util/localStorage.ts
@@ -1,9 +1,20 @@
+// Reusable functions, exported just in case
 export const LS = {
   set(key: string, value: any) {
     localStorage.setItem(key, JSON.stringify(value));
   },
-
   get(key: string) {
-    return JSON.parse(localStorage.getItem(key) || "");
+    const raw = localStorage.getItem(key) || "{}"; // Returns string of LS key
+    const parsed = JSON.parse(raw); // Returns parsed object
+    return raw === "{}" ? null : parsed; // Returns null if empty
   },
+};
+
+export const setAnimalsOnLoad = async () => {
+  const animals = LS.get("animals");
+  if (animals) return;
+
+  const response = await fetch("https://animals.azurewebsites.net/api/animals");
+  const data = await response.json();
+  LS.set("animals", data);
 };
